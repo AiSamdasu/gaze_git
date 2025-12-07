39e015dc213e328f35a0028ab5a288e102d054c5
kimtaekyu <kimtaekyu@localhost>
2025-12-07 14:50:35 +0900

    3

--- gaze_git_java/src/gaze_git_java/C.java
+++ gaze_git_java/src/gaze_git_java/C.java
@@ -1,5 +1,11 @@
 package gaze_git_java;
 
 public class C {
-
+	public static void main(String[] args) {
+		System.out.println("master branch에서 변경");
+		System.out.println("Alice's change in C class");
+		A a = new A();
+		System.out.println("Bob changed C class.");
+		B b = new B();
+	}
 }
